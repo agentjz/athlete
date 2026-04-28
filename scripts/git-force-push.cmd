85189ae8587c6e217a98f3da88bdaa0e84f3b380
Legion-jun <1773384983@qq.com>
2026-04-28 16:03:05 +0800

    Use SSH remote in force push script

--- scripts/git-force-push.cmd
+++ scripts/git-force-push.cmd
@@ -3,7 +3,12 @@ chcp 65001 >nul
 cd /d "%~dp0\.."
 set /p message=Commit message:
 git init
-git remote add origin https://github.com/agentjz/deadmouse-agent
+git remote get-url origin >nul 2>nul
+if errorlevel 1 (
+  git remote add origin git@github.com:agentjz/deadmouse-agent.git
+) else (
+  git remote set-url origin git@github.com:agentjz/deadmouse-agent.git
+)
 git add .
 git commit -m "%message%"
 git push origin master --force
